fa1856b1d9a254060734466850e21a602789f797
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-02-28 13:02:37 +0000

    update UserController to manage user profiles; add methods for profile creation, retrieval, and user mobility summary

--- src/main/java/com/mobilitypass/user_mobility/controller/UserController.java
+++ src/main/java/com/mobilitypass/user_mobility/controller/UserController.java
@@ -1,26 +1,43 @@
 package com.mobilitypass.user_mobility.controller;
 
-import com.mobilitypass.user_mobility.beans.MobilityPass;
-import com.mobilitypass.user_mobility.beans.User;
-import com.mobilitypass.user_mobility.dto.UserRegistrationDTO;
+import com.mobilitypass.user_mobility.beans.UserProfile;
+import com.mobilitypass.user_mobility.dto.UserMobilitySummaryDTO;
+import com.mobilitypass.user_mobility.dto.UserProfileDTO;
 import com.mobilitypass.user_mobility.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Contrôleur pour la gestion des profils utilisateurs.
+ * Ce contrôleur permet de créer un profil métier lié à un ID Keycloak
+ * et de récupérer les informations d'un utilisateur.
+ */
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    @Autowired private UserService userService;
+    @Autowired
+    private UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody UserRegistrationDTO dto) {
-        return ResponseEntity.ok(userService.registerUser(dto));
+    @GetMapping("/me")
+    public ResponseEntity<UserProfile> getMe(@AuthenticationPrincipal Jwt jwt) {
+        return ResponseEntity.ok(userService.getOrCreateProfile(jwt));
     }
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
 
-        User user = userService.getUserById(id);
-        return ResponseEntity.ok(user);
+    @GetMapping("/summary/{keycloakId}")
+    public ResponseEntity<UserMobilitySummaryDTO> getSummary(@PathVariable String keycloakId) {
+        return ResponseEntity.ok(userService.getSummary(keycloakId));
+    }
+
+    @PostMapping("/profile")
+    public ResponseEntity<UserProfile> createProfile(@RequestBody UserProfileDTO dto) {
+        return ResponseEntity.ok(userService.createProfile(dto));
+    }
+
+    @GetMapping("/{keycloakId}")
+    public ResponseEntity<UserProfile> getUser(@PathVariable String keycloakId) {
+        return ResponseEntity.ok(userService.getUser(keycloakId));
     }
 }
