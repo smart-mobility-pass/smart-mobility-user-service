c74dc4180ee78c3466a6a3eae0b1ef9586181a0d
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-02-28 13:02:27 +0000

    update SubscriptionController to add method for retrieving active subscriptions; refactor create method to use subscriptionType

--- src/main/java/com/mobilitypass/user_mobility/controller/SubscriptionController.java
+++ src/main/java/com/mobilitypass/user_mobility/controller/SubscriptionController.java
@@ -1,17 +1,17 @@
 package com.mobilitypass.user_mobility.controller;
 
-
 import com.mobilitypass.user_mobility.beans.Subscriptions;
 import com.mobilitypass.user_mobility.service.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.concurrent.Flow;
+import java.util.List;
 
+/**
+ * Contrôleur pour la gestion des abonnements.
+ * Gère la création et la récupération des abonnements actifs d'un utilisateur.
+ */
 @RestController
 @RequestMapping("/api/subscriptions")
 public class SubscriptionController {
@@ -22,9 +22,13 @@ public class SubscriptionController {
     public ResponseEntity<Subscriptions> create(@RequestBody Subscriptions sub) {
         Subscriptions savedSub = subscriptionService.createSubscription(
                 sub.getUserId(),
-                sub.getType(),
-                sub.getDiscountPercentage()
-        );
+                sub.getSubscriptionType(),
+                sub.getDiscountPercentage());
         return ResponseEntity.ok(savedSub);
+    }
+
+    @GetMapping("/active/{userId}")
+    public ResponseEntity<List<Subscriptions>> getActiveSubscriptions(@PathVariable String userId) {
+        return ResponseEntity.ok(subscriptionService.getActiveSubscriptions(userId));
     }
 }
