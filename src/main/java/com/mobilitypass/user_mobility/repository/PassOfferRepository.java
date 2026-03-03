6a34fbaee1c01a7b2cd9486ba0c89c0aa9370465
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-03-03 10:05:09 +0000

    Add repositories for PassOffer and SubscriptionOffer entities

---
+++ src/main/java/com/mobilitypass/user_mobility/repository/PassOfferRepository.java
@@ -0,0 +1,7 @@
+package com.mobilitypass.user_mobility.repository;
+
+import com.mobilitypass.user_mobility.beans.PassOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassOfferRepository extends JpaRepository<PassOffer, Long> {
+}
---
+++ src/main/java/com/mobilitypass/user_mobility/repository/SubscriptionOfferRepository.java
@@ -0,0 +1,7 @@
+package com.mobilitypass.user_mobility.repository;
+
+import com.mobilitypass.user_mobility.beans.SubscriptionOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriptionOfferRepository extends JpaRepository<SubscriptionOffer, Long> {
+}
